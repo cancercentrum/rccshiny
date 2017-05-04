094709f83c56c04594db7fce00ddb4c44f220a05
Lina Enqvist Benson(552l) <SE2321000016-552L@sll.se>
2017-05-04 09:57:13 +0200

    added documentation + changed searchpaths

--- R/rccShiny.R
+++ R/rccShiny.R
@@ -1,7 +1,134 @@
-rccShiny <- function(data = NULL, outcome = "outcome", outcomeTitle = outcome, folder = "ind", folderLinkText = outcomeTitle, titleTextBeforeSubtitle = NULL, titleTextAfterSubtitle = NULL,
-    comment = "", description = c("(beskrivning saknas)", "(description missing)"), geoUnitsHospital = "sjukhus", geoUnitsCounty = "landsting", geoUnitsRegion = "region", geoUnitsFromLKF = FALSE,
-    regionSelection = TRUE, regionLabel = c("Begränsa till region", "Limit to region"), period = "period", periodLabel = c("Diagnosår", "Year of diagnosis"), userInputList = NULL,
-    targetValues = NULL, targetValuesSortDescending = NULL, hideLessThan = 5, language = c("sv"), npcrGroupPrivateOthers = TRUE) {
+#' Shiny apps for RCC
+#' @description returns shiny apps used as a complement to the annual reports from the cancer quality registries in Sweden.
+#'
+#' @param data data frame containing the variables used in the app.
+#' @param outcome vector with the name(s) of the variable(s) of interest, for example a "kvalitetsindikator". Default is "outcome".
+#' @param outcomeTitle label(s) of the outcome(s). Must be the same lenght as argument outcome. Default is argument outcome (name(s) of the outcome(s) in the dataset).
+#' @param folder name of folder where the results are placed.
+#' @param folderLinkText short name used in html lank??? glunkiglunk. Fredrik: HJÄLP SÖKES!
+#' @param path searchpath to folder
+#' @param titleTextBeforeSubtitle optional text placed before the subtitles in the tabs.
+#' @param titleTextAfterSubtitle optional text placed after the subtitles in the tabs.
+#' @param comment optional comment printed under the sidebar panel.
+#' @param description description shown in the tab Beskrivning/Description.
+#' @param geoUnitsHospital optional character vector with hospital names. To be implemented: Hospital codes.
+#' @param geoUnitsCounty optional numeric vector with county codes. Can be either county of residence for the patient or the county the hospital belongs to. To be implemented: Codes for county of hospital are fetched automatically from hospital codes.
+#' @param geoUnitsRegion FREDRIK: ska denna vara optional? numeric vector with region codes (1=Stockholm, 2=Uppsala-Örebro, 3=Sydöstra, 4=Södra, 5=Västra, 6=Norra). To be implemented: Codes for region of hospital are fetched automatically from hospital codes.
+#' @param geoUnitsFromLKF if geoUnitsCounty is county of residence for the patient (LKF). If FALSE and a hospital is choosen by the user in the sidebar panel the output is highlighted for the respective county/region that the hospital belongs to. Default is FALSE.
+#' @param regionSelection adds a widget to the sidebar panel with the option to show only one region at a time. Default is TRUE.
+#' @param regionLabel used if regionSelection = TRUE. Label of widget shown in the sidebar panel. Default is c("Begränsa till region", "Limit to region").
+#' @param period numeric vector with time periods, for example year of diagnosis. Default is "period".
+#' @param periodLabel label for the period widget in the sidebar panal. Default is c("Diagnosår", "Year of diagnosis").
+#' @param userInputList list with variables (other than period and geoUnits) to be shown in sidebar panel. Arguments to the list are: var (variable name in dataset), label (label shown over widget in sidebar panel), choices (which values of var should be shown, min, max for continous variables).
+#' @param targetValues optional vector of 1-2 targetvalues ("målnivåer") to be plotted.
+#' @param targetValuesSortDescending should the bars in tab Jämförelse/Comparison be plotted in descending order. Deafault is TRUE.
+#' @param hideLessThan optional value under which groups (cells) are supressed. Default is 5 and all values < 5 are set to 5.
+#' @param language vector giving the language for the app. Possible values are "sv" and "en". Default is "sv". See details.
+#' @param npcrGroupPrivateOthers Applicable for NPCR only. FREDRIK: Ska default vara TRUE?
+#'
+#' @details Valid values for geoUnitsCounty are:
+#'   \tabular{lll}{
+#' geoUnitsFromLKF   \tab !geoUnitsFromLKF \tab Text\cr
+#' 1\tab 10,11 \tab  Stockholm\cr
+#' 3\tab 12 \tab  Uppsala\cr
+#' 4\tab 13    \tab  Södermanland\cr
+#' 5\tab 21    \tab  Östergötland\cr
+#' 6\tab 22     \tab Jönköping\cr
+#' 7\tab 23     \tab  Kronoberg\cr
+#' 8\tab 24,25   \tab  Kalmar\cr
+#' 9\tab 26 \tab  Gotland\cr
+#' 10\tab 27   \tab  Blekinge\cr
+#' 12\tab 28,30,41   \tab  Skåne\cr
+#' 13\tab 42   \tab  Halland\cr
+#' 14\tab 50,51,52,53   \tab  Västra Götaland\cr
+#' 17\tab 54   \tab  Värmland\cr
+#' 18\tab 55   \tab  Örebro\cr
+#' 19\tab 56   \tab  Västmanland\cr
+#' 20\tab 57   \tab  Dalarna\cr
+#' 21\tab 61   \tab  Gävleborg\cr
+#' 22\tab 62   \tab  Västernorrland\cr
+#' 23\tab 63   \tab  Jämtland\cr
+#' 24\tab 64   \tab  Västerbotten\cr
+#' 25\tab 65   \tab  Norrbotten\cr
+#' -\tab 91,92,93,94,95,96   \tab  Övriga/privata - region
+#' }
+#'
+#'
+#'
+#' If language = c("sv", "en") the following applies to arguments: outcomeTitle, titleTextBeforeSubtitle, titleTextAfterSubtitle, comment, description,
+#' regionLabel, label in list userInputList: if there are two values the first is used in the swedish version and the second in the english version. These must be given in order sv, en. If there is only one value this is recycled in both versions.
+#' The following applies to arguments outcome, geoUnitsHospital, geoUnitsCounty, geoUnitsRegion, period, var in list userInputList: in the english version rccShiny will use the variable name with the suffix _en (for example "outcome_en") if this exists and otherwise recycle the swedish variable name.
+#'
+#' @author Fredrik Sandin, RCC Uppsala-Örebro
+#'
+#' @return A folder path/apps/sv|en/folder containing: global.R, server.R, ui.R, data/data.RData, docs/description.html.
+#' @examples
+#' rccShiny(
+#'   data = rccShinyData,
+#'   folder = "Indikator1",
+#'   outcome = paste0("outcome",1:3),
+#'   outcomeTitle = c("Dikotom", "Kontinuerlig", "Kategorisk"),
+#'   comment = "Skövde och Lidköping tillhör Skaraborg",
+#'   description = "Ett fall per bröst kan ha rapporterats till det nationella kvalitetsregistret för bröstcancer.
+#'   Det innebär att samma person kan finnas med i statistiken upp till två gånger.",
+#'   userInputList = list(
+#'     list(
+#'       var = "age",
+#'       label = "Ålder vid diagnos"
+#'     ),
+#'     list(
+#'       var = "stage",
+#'       label = "Stadium",
+#'       choices = c("I", "II")
+#'     )
+#'   )
+#' )
+#'
+#' # For Swedish/English version
+#' rccShinyData$outcome_en <- rccShinyData$outcome
+#' rccShiny(
+#'   data = rccShinyData,
+#'   folder = "Indikator2",
+#'   outcome = "outcome",
+#'   outcomeTitle = c("Kontaktsjuksköterska", "Contact nurse"),
+#'   titleTextBeforeSubtitle = c("Något på svenska","Something in English"),
+#'   description = c("Superbra att ha!","Supergood to have!"),
+#'   userInputList = list(
+#'     list(
+#'       var = "age",
+#'       label = c("Ålder vid diagnos","Age at diagnosis"),
+#'       choices = c(0,120)
+#'     )
+#'   ),
+#'   targetValues = c(95,99),
+#'   language = c("sv", "en")
+#' )
+#' @export
+
+rccShiny <- function(data = NULL,
+                     outcome = "outcome",
+                     outcomeTitle = outcome,
+                     folder = "ind",
+                     folderLinkText = outcomeTitle,
+                     path = "",
+                     titleTextBeforeSubtitle = NULL,
+                     titleTextAfterSubtitle = NULL,
+                     comment = "",
+                     description = c("(beskrivning saknas)", "(description missing)"),
+                     geoUnitsHospital = "sjukhus",
+                     geoUnitsCounty = "landsting",
+                     geoUnitsRegion = "region",
+                     geoUnitsFromLKF = FALSE,
+                     regionSelection = TRUE,
+                     regionLabel = c("Begränsa till region", "Limit to region"),
+                     period = "period",
+                     periodLabel = c("Diagnosår", "Year of diagnosis"),
+                     userInputList = NULL,
+                     targetValues = NULL,
+                     targetValuesSortDescending = NULL,
+                     hideLessThan = 5,
+                     language = c("sv"),
+                     npcrGroupPrivateOthers = TRUE) {
 
     # # # # # # # # # # # # # # # # Lägg till felkontroller!  # # # # # # # # # # # # # # #
 
@@ -224,22 +351,25 @@ rccShiny <- function(data = NULL, outcome = "outcome", outcomeTitle = outcome, f
 
         GLOBAL_npcrGroupPrivateOthers <- npcrGroupPrivateOthers
 
-        if (!dir.exists(paste0("./apps/", loop_language))) {
-            dir.create(paste0("./apps/", loop_language), showWarnings = FALSE)
+        if (!dir.exists(paste0(path,"/apps/"))) {
+          dir.create(paste0(path,"/apps/"), showWarnings = FALSE)
+        }
+        if (!dir.exists(paste0(path,"/apps/", loop_language))) {
+            dir.create(paste0(path,"/apps/", loop_language), showWarnings = FALSE)
         }
 
-        dir.create(paste0("./apps/", loop_language, "/", folder), showWarnings = FALSE)
-        dir.create(paste0("./apps/", loop_language, "/", folder, "/data"), showWarnings = FALSE)
-        dir.create(paste0("./apps/", loop_language, "/", folder, "/docs"), showWarnings = FALSE)
+        dir.create(paste0(path, "/apps/", loop_language, "/", folder), showWarnings = FALSE)
+        dir.create(paste0(path, "/apps/", loop_language, "/", folder, "/data"), showWarnings = FALSE)
+        dir.create(paste0(path, "/apps/", loop_language, "/", folder, "/docs"), showWarnings = FALSE)
 
-        file.copy("./source/global.R", paste0("./apps/", loop_language, "/", folder, "/global.R"), overwrite = TRUE)
-        file.copy("./source/server.R", paste0("./apps/", loop_language, "/", folder, "/server.R"), overwrite = TRUE)
-        file.copy("./source/ui.R", paste0("./apps/", loop_language, "/", folder, "/ui.R"), overwrite = TRUE)
+        file.copy(system.file("source", "global.R", package = "rccShiny"), paste0(path, "/apps/", loop_language, "/", folder, "/global.R"), overwrite = TRUE)
+        file.copy(system.file("source", "server.R", package = "rccShiny"), paste0(path, "/apps/", loop_language, "/", folder, "/server.R"), overwrite = TRUE)
+        file.copy(system.file("source", "ui.R", package = "rccShiny"), paste0(path, "/apps/", loop_language, "/", folder, "/ui.R"), overwrite = TRUE)
 
         save(GLOBAL_data, GLOBAL_outcome, GLOBAL_outcomeTitle, GLOBAL_outcomeClass, GLOBAL_titleTextBeforeSubtitle, GLOBAL_titleTextAfterSubtitle, GLOBAL_comment, GLOBAL_description,
             GLOBAL_periodLabel, GLOBAL_periodStart, GLOBAL_periodEnd, GLOBAL_geoUnitsFromLKF, GLOBAL_regionSelection, GLOBAL_regionLabel, GLOBAL_regionChoices, GLOBAL_regionSelected,
-            GLOBAL_targetValues, GLOBAL_targetValuesSortDescending, GLOBAL_userInputList, GLOBAL_hideLessThan, GLOBAL_language, GLOBAL_npcrGroupPrivateOthers, file = paste0("./apps/",
-                loop_language, "/", folder, "/data/data.RData"))
+            GLOBAL_targetValues, GLOBAL_targetValuesSortDescending, GLOBAL_userInputList, GLOBAL_hideLessThan, GLOBAL_language, GLOBAL_npcrGroupPrivateOthers,
+            file = paste0(path,"/apps/", loop_language, "/", folder, "/data/data.RData"))
 
         # Output description to .html-file
 
@@ -247,7 +377,7 @@ rccShiny <- function(data = NULL, outcome = "outcome", outcomeTitle = outcome, f
             cat(paste0(row, "\n"), file = file, append = append)
         }
 
-        globalOutFile <<- file(paste0("./apps/", loop_language, "/", folder, "/docs/description.html"), "w", encoding = "UTF-8")
+        globalOutFile <<- file(paste0(path,"/apps/", loop_language, "/", folder, "/docs/description.html"), "w", encoding = "UTF-8")
         printRow(paste0("<!DOCTYPE html>"), append = FALSE)
         printRow(paste0("<html>"))
         printRow(paste0("<body>"))
