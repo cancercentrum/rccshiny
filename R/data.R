c8ff55ce7ec250db15952520e40d72434d838126
Lina Enqvist Benson(552l) <SE2321000016-552L@sll.se>
2017-05-04 14:26:34 +0200

    modified documentation.

--- R/data.R
+++ R/data.R
@@ -4,14 +4,14 @@
 #'
 #' @format A data frame with 20000 rows and 9 variables:
 #' \describe{
-#'   \item{sjukhus}{A character vector contining hospitals names.}
-#'   \item{landsting}{A numeric vector containg codes for the 'landsting' for the respective hospitals.}
-#'   \item{region}{A numeric vector containg codes (1-6) for the region for the respective hospitals.}
-#'   \item{period}{Year of diagnosis (2012-2016).}
-#'   \item{age}{Patients age at diagnosis.}
-#'   \item{outcome1}{Dichotomous outcome (logical: TRUE/FALSE).}
-#'   \item{outcome2}{Continuous outcome (numeric).}
-#'   \item{outcome3}{Categorical outcome with 5 levels (factor).}
-#'   \item{stage}{Patients stage at diagnosis.}
+#'   \item{sjukhus}{a character vector contining hospitals names.}
+#'   \item{landsting}{a numeric vector containg codes for the 'landsting' for the respective hospitals.}
+#'   \item{region}{a numeric vector containg codes (1-6) for the region for the respective hospitals.}
+#'   \item{period}{year of diagnosis (2012-2016).}
+#'   \item{age}{patients age at diagnosis.}
+#'   \item{outcome1}{dichotomous outcome (logical: TRUE/FALSE).}
+#'   \item{outcome2}{continuous outcome (numeric).}
+#'   \item{outcome3}{categorical outcome with 5 levels (factor).}
+#'   \item{stage}{patients stage at diagnosis.}
 #' }
 "rccShinyData"
