d5b25e842531c138018b147aeef2bffb733be661
John Lövrot <john.lovrot@sll.se>
2020-09-29 16:20:06 +0200

    Skip on Appveyor and Travis, for now

--- tests/testthat/test-sampleapp-file.R
+++ tests/testthat/test-sampleapp-file.R
@@ -4,6 +4,12 @@ test_that("sampleapp works", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
 
+  # Skip on Appveyor, for now
+  skip_on_appveyor()
+
+  # Skip on Travis, for now
+  skip_on_travis()
+
   # Use compareImages = FALSE because (potential) expected image screenshots
   # were (probably) created on a Windows system, and they will differ from
   # screenshots taken on a CI platform that runs on Linux.
