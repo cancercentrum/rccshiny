96042d9e82fa3f104e67c507d3dcb1476afcc43a
John Lövrot <john.lovrot@sll.se>
2020-09-30 14:42:54 +0200

    Add shinytest of running legacyapp-1.3

---
+++ tests/testthat/test-running-legacyapp-1.3.R
@@ -0,0 +1,12 @@
+library(shinytest)
+
+test_that("running legacyapp-1.5.1 works on windows", {
+  skip_on_travis()
+  skip_on_appveyor()
+
+  # Skip on other OS than Windows
+  skip_on_os(c("mac", "linux", "solaris"))
+
+  appdir <- system.file(package = "rccShiny", file.path("testapps", "sv", "legacyapp-1.5.1"))
+  shinytest::expect_pass(testApp(appdir, compareImages = FALSE, suffix = "windows"))
+})
