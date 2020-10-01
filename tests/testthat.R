8bf4d47d3597acbc7364cfdd61d5ee246d2772bb
John Lövrot <john.lovrot@sll.se>
2020-10-01 09:02:54 +0200

    Rätta fel

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,7 @@ library(testthat)
 library(rccShiny)
 
 # Adopted from https://github.com/rstudio/webdriver/blob/appveyor/tests/testthat.R
-if (is.null(webdriver:::find_phantom())) install_phantomjs()
+if (is.null(webdriver:::find_phantom())) webdriver::install_phantomjs()
 cat("Using phantom.js from", webdriver:::find_phantom(), "\n")
 
 test_check("rccShiny")
