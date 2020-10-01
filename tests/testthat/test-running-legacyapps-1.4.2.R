9bb971b4fdf6f11b57a1c94272d9659f254aa470
John Lövrot <john.lovrot@sll.se>
2020-10-01 10:18:23 +0200

    Add tests on Mac

--- tests/testthat/test-running-legacyapps-1.4.2.R
+++ tests/testthat/test-running-legacyapps-1.4.2.R
@@ -10,3 +10,14 @@ test_that("running legacyapp1-1.4.2 works on windows", {
   appdir <- system.file(package = "rccShiny", file.path("testapps", "sv", "legacyapp1-1.4.2"))
   expect_pass(testApp(appdir, "mytest", quiet = TRUE, compareImages = FALSE, interactive = FALSE, suffix = "windows"))
 })
+
+test_that("running legacyapp1-1.4.2 works on mac", {
+  skip_on_travis()
+  skip_on_appveyor()
+
+  # Skip on other OS than Mac
+  skip_on_os(c("windows", "linux", "solaris"))
+
+  appdir <- system.file(package = "rccShiny", file.path("testapps", "sv", "legacyapp1-1.4.2"))
+  expect_pass(testApp(appdir, "mytest", quiet = TRUE, compareImages = FALSE, interactive = FALSE, suffix = "mac"))
+})
