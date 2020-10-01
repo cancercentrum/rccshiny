3d988eb8e6c042fb7a1420418be0d1dd9ce2e8d4
John Lövrot <john.lovrot@sll.se>
2020-10-01 12:31:48 +0200

    Try add shinytest on linux

--- tests/testthat/test-running-legacyapps-1.5.1.R
+++ tests/testthat/test-running-legacyapps-1.5.1.R
@@ -1,5 +1,16 @@
 library(shinytest)
 
+test_that("running legacyapp1-1.5.1 works on linux", {
+  # skip_on_travis()
+  # skip_on_appveyor()
+
+  # Skip on other OS than Linux
+  skip_on_os(c("windows", "mac", "solaris"))
+
+  appdir <- file.path("apps", "sv", "legacyapp1-1.5.1")
+  expect_pass(testApp(appdir, quiet = TRUE, compareImages = FALSE, suffix = "linux"))
+})
+
 test_that("running legacyapp1-1.5.1 works on windows", {
   skip_on_travis()
   # skip_on_appveyor()
