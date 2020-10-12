5eb32a3df729b29adb809c9f66237bf907a15049
John Lövrot <john.lovrot@sll.se>
2020-10-12 09:58:28 +0200

    Add workaround to mimic an INCA-like situation in tests

---
+++ tests/testthat/apps/sv/inca1hc/app.R
@@ -0,0 +1,29 @@
+library(rccShiny)
+
+# Adopted from https://bitbucket.org/cancercentrum/rccshiny/src/1.6.0/R/rccShiny2.R#lines-94
+# with outputHighcharts = TRUE
+rccShiny2(
+  inca = TRUE,
+  incaScript = system.file(package = "rccShiny", file.path("scripts", "inca-dm.R")),
+  incaIncludeList = TRUE,
+  id = "id",
+  idAuthorisedToView = "idAuthorisedToView",
+  outcome = paste0("outcome", 1:3),
+  outcomeTitle = c("Dikotom", "Kontinuerlig", "Kategorisk"),
+  description = c("Har beskrivs indikatorn.", "Viktig information!", "Information om variabler etc."),
+  varOther = list(
+    list(
+      var = "age",
+      label = "Alder vid diagnos"
+    ),
+    list(
+      var = "stage",
+      label = "Stadium",
+      choices = c("I", "II"),
+      selected = "I",
+      multiple = TRUE,
+      showInTitle = TRUE
+    )
+  ),
+  outputHighcharts = TRUE
+)
