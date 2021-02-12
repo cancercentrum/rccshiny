c70663f00e47a555c087ffaec5b3e67435690665
John Lövrot <john.lovrot@sll.se>
2021-02-12 11:10:07 +0100

    Add instruction/comment

--- tools/update-shinytests-expected-wincheck-2.R
+++ tools/update-shinytests-expected-wincheck-2.R
@@ -3,6 +3,10 @@
 # 1. Run update-shinytests-expected-wintest.R
 # 2. Run update-shinytests-expected-wincheck-1.R
 # 3. Run Check Package in RStudio
+# 4. Inspect differences between shinytest results when running tests during
+#    R CMD check and expected shinytest results when testing the package in
+#    RStudio (e.g. Ctrl+Shift+T, devtools::test, testthat::test_check or
+#    testthat::test_dir) -- the differences should be minor
 
 dirs_expected_wincheck <- stringr::str_subset(
   list.dirs("../rccShiny.Rcheck/tests/testthat/apps/"),
