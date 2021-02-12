03af6a6c6f451b1af0449e1606eec8aa2265e830
John Lövrot <john.lovrot@sll.se>
2021-02-12 10:16:17 +0100

    Simplify handling of different expected shinytest results on Windows

--- tests/testthat/helper-shinytest.R
+++ tests/testthat/helper-shinytest.R
@@ -7,21 +7,7 @@ shinytest_suffix <- function() {
 
   if (sysname == "windows") {
     # Use different expected shinytest results on Windows for different scenarios
-    # - testthat::test_dir, use "windows" (rename to "wintest"?)
-    # - devtools::test, use "windows" (rename to "wintest"?)
-    # - devtools::check, use "wincheck"
-    # - rcmdcheck::rcmdcheck, use "wincheck"
-
-    if (identical(Sys.getenv("NOT_CRAN"), "true")) {
-      # Probably running devtools since NOT_CRAN is set to "true",
-      if (identical(Sys.getenv("_R_CHECK_FORCE_SUGGESTS_"), "FALSE")) {
-        # Probably running devtools::check since _R_CHECK_FORCE_SUGGESTS_ is set to FALSE
-        suffix <- "wincheck"
-      } else {
-        # Probably running another devtools function, e.g. devtools::test()
-        suffix <- "windows"
-      }
-    } else if (identical(Sys.getenv("R_CMD"), "R CMD")) {
+    if (identical(Sys.getenv("R_CMD"), "R CMD")) {
       # Probably running R CMD check
       suffix <- "wincheck"
     } else {
