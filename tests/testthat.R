0cb93c040a424f88ff3fda738a811107d07cf7ef
John Lövrot <john.lovrot@sll.se>
2021-02-12 07:07:01 +0100

    Use different expected shinytest results on Windows for different scenarios - testthat::test_dir, use "windows" (rename to "wintest"?) - devtools::test, use "windows" (rename to "wintest"?) - devtools::check, use "wincheck" - rcmdcheck::rcmdcheck, use "wincheck"

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,7 +6,10 @@ if (is.null(webdriver:::find_phantom())) webdriver::install_phantomjs()
 cat("Using phantom.js from", webdriver:::find_phantom(), "\n")
 
 # For diagnostics
+cat(paste0("Sys.info()[['sysname'']]: ", Sys.info()[["sysname"]], "\n"))
 cat(paste0("Sys.getenv('NOT_CRAN'): ", Sys.getenv("NOT_CRAN"), "\n"))
+cat(paste0("Sys.getenv('_R_CHECK_FORCE_SUGGESTS_'): ", Sys.getenv("_R_CHECK_FORCE_SUGGESTS_"), "\n"))
+cat(paste0("Sys.getenv('R_TESTS'): ", Sys.getenv("R_TESTS"), "\n"))
 cat(paste0("packageVersion('shinyWidgets'): ", packageVersion("shinyWidgets"), "\n"))
 
 test_check("rccShiny", reporter = "minimal")
--- tests/testthat/helper-shinytest.R
+++ tests/testthat/helper-shinytest.R
@@ -3,13 +3,28 @@ file_remove_if_exists <- function(x) {
 }
 
 shinytest_suffix <- function() {
-
   sysname <- tolower(Sys.info()[["sysname"]])
 
-  if (identical(Sys.getenv("APPVEYOR"), "True")) {
-    suffix <- "wincheck"
-  } else if (sysname == "windows") {
+  if (sysname == "windows") {
+    # Use different expected shinytest results on Windows for different scenarios
+    # - testthat::test_dir, use "windows" (rename to "wintest"?)
+    # - devtools::test, use "windows" (rename to "wintest"?)
+    # - devtools::check, use "wincheck"
+    # - rcmdcheck::rcmdcheck, use "wincheck"
+
     if (identical(Sys.getenv("NOT_CRAN"), "true")) {
+      # Probably running devtools since NOT_CRAN is set to "true",
+      if (identical(Sys.getenv("_R_CHECK_FORCE_SUGGESTS_"), "FALSE")) {
+        # Probably running devtools::check since _R_CHECK_FORCE_SUGGESTS_ is set to FALSE
+        suffix <- "wincheck"
+      } else {
+        # Probably running another devtools function, e.g. devtools::test()
+        suffix <- "windows"
+      }
+    } else if (!identical(Sys.getenv("R_TESTS"), "")) {
+      # Probably running R CMD check
+      # When R CMD check runs tests, it sets R_TESTS.
+      # see https://github.com/r-lib/devtools/blob/v2.3.2/R/R.R#L17
       suffix <- "wincheck"
     } else {
       suffix <- "windows"
