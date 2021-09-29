14d69d135af1ddb0fd983af022f9d8286eeb9e7c
Fredrik Sandin <saf011@lul-net.ad.lul.se>
2021-09-29 07:53:44 +0200

    SNY-105: Fixed bug where app crashed if outcome was NA and a group was smaller than hideLessThan.

--- R/rcc2PlotInd.R
+++ R/rcc2PlotInd.R
@@ -245,7 +245,7 @@ rcc2PlotInd <-
           hide <- tempN < groupHideLessThan
           hideCellLessThan <- FALSE
           if (hide) {
-            measurements <- data.frame(NA, NA)
+            measurements <- data.frame(NA, NA, NA, NA)
           } else {
             measurements <- data.frame(tempN, tempN, tempN, tempN)
           }
